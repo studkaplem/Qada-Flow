5dc0b47f2bc7d8d2b1905338479a9b340278081e
Emre Kaplan <emre.kaplan@stud.th-rosenheim.de>
2026-02-01 00:32:40 +0100

    add anaytics

--- src/app.component.ts
+++ src/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { TranslationService } from './services/translation.service';
 import { AuthService } from './services/auth.service';
 import { TranslatePipe } from './pipes/translate.pipe';
+import { inject as injectAnalytics } from '@vercel/analytics';
 
 @Component({
   selector: 'app-root',
@@ -14,6 +15,10 @@ export class AppComponent {
   ts = inject(TranslationService);
   auth = inject(AuthService); // Injected to check login state
   isMobileMenuOpen = false;
+
+  constructor() {
+    injectAnalytics();
+  }
 
   toggleMenu() {
     this.isMobileMenuOpen = !this.isMobileMenuOpen;
